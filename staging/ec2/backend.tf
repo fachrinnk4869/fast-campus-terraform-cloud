a7ea9d6bb76950f4b26f6403913ec3bb0db37344
fachri <fachrinajmnoer@upi.edu>
2024-10-03 06:33:31 +0700

    change aws_configure

--- staging/ec2/backend.tf
+++ staging/ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket         = "practice-remote-states"
+        bucket         = "practice-remote-states-2"
         key            = "staging/ec2"
         region         = "us-east-1"
         encrypt        = true
